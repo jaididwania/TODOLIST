ed83bc16b7de06fa9a5c5ae2a1e09466e9daa9a8
Jaikishan Didwania <jaididwania@gmail.com>
2020-06-04 14:56:05 +0530

    added delete

--- TODO/urls.py
+++ TODO/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns=[
     path('',views.home,name="home"),
     path('add_todo/',views.add_todo,name='add_todo'),
+    path('delete_todo/<int:todo_id>/', views.delete_todo,name="delete_todo")
 ]
--- TODO/views.py
+++ TODO/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.utils import timezone
+from TODO.models import TODO
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return render(request,'main/index.html')
+    #grabbing todo items from database
+    todo_items = TODO.objects.all().order_by("-added_date")
+    context = {
+        "todo_items": todo_items
+    }
+    return render(request,'main/index.html', context)
 
 def add_todo(request):
     
-    added_date = timezone.now()
+    current_date = timezone.now()
     content = request.POST['content']
-    print(added_date)
-    print(content)
-    return render(request,'main/index.html')
+    created_obj = TODO.objects.create(added_date = current_date,text= content)
+
+    return HttpResponseRedirect("/")
+
+def delete_todo(request,todo_id):
+    print(todo_id)
+    return HttpResponseRedirect("/")
