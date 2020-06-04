2c12ca8c81f5686021b38e108ac6403f745f3179
Jaikishan Didwania <jaididwania@gmail.com>
2020-06-04 14:08:40 +0530

    progress

--- TODO/urls.py
+++ TODO/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+
 urlpatterns=[
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    path('add_todo/',views.add_todo,name='add_todo'),
 ]
--- TODO/views.py
+++ TODO/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 def home(request):
-    return render(request,'base.html')
+    return render(request,'main/index.html')
+
+def add_todo(request):
+    
+    added_date = timezone.now()
+    content = request.POST['content']
+    print(added_date)
+    print(content)
+    return render(request,'main/index.html')
